ab456b46f4a3afd5862d44478cc8097c998cafc1
ortariot <ortariot@gmail.com>
2021-06-10 18:38:42 +0700

    sintacsis edit

--- main.py
+++ main.py
@@ -48,35 +48,31 @@ class WindowsForms(QtWidgets.QMainWindow, vd_design.Ui_MainWindow, ValveGPIO):
         self.set_duty(valve_num, value_vlv1)
 
     def slider_vlv1(self):
-        self.slaide_action(self, 1, self.vlv1_verticalSlider,
-                           self.vlv1_lcdNumber)
+        self.slaide_action(1, self.vlv1_verticalSlider, self.vlv1_lcdNumber)
         # value_vlv1 = self.vlv1_verticalSlider.value()
         # self.vlv1_lcdNumber.display(value_vlv1)
         # self.set_duty(1, value_vlv1)
 
     def slider_vlv2(self):
-        self.slaide_action(self, 2, self.vlv1_verticalSlider,
-                           self.vlv1_lcdNumber)
+        self.slaide_action(2, self.vlv1_verticalSlider, self.vlv1_lcdNumber)
         # value_vlv2 = self.vlv2_verticalSlider.value()
         # self.vlv2_lcdNumber.display(value_vlv2)
         # self.set_duty(2, value_vlv2)
 
     def slider_vlv3(self):
-        self.slaide_action(self, 3, self.vlv1_verticalSlider,
-                           self.vlv1_lcdNumber)
+        self.slaide_action(3, self.vlv1_verticalSlider, self.vlv1_lcdNumber)
         # value_vlv3 = self.vlv3_verticalSlider.value()
         # self.vlv3_lcdNumber.display(value_vlv3)
         # self.set_duty(3, value_vlv3)
 
     def slider_vlv4(self):
-        self.slaide_action(self, 4, self.vlv1_verticalSlider,
-                           self.vlv1_lcdNumber)
+        self.slaide_action(4, self.vlv1_verticalSlider, self.vlv1_lcdNumber)
         # value_vlv4 = self.vlv4_verticalSlider.value()
         # self.vlv4_lcdNumber.display(value_vlv4)
         # self.set_duty(4, value_vlv4)
 
     def btn_vlv1(self):
-        self.button_action(self, 1, self.vlv1_button, self.vlv1_verticalSlider)
+        self.button_action(1, self.vlv1_button, self.vlv1_verticalSlider)
         # if self.vlv1_button.text() == "VALVE1 ON":
         #     # self.sw_start(1, self.vlv1_verticalSlider.value())
         #     self.sw_init(1)
@@ -88,7 +84,7 @@ class WindowsForms(QtWidgets.QMainWindow, vd_design.Ui_MainWindow, ValveGPIO):
         #     self.vlv1_verticalSlider.setEnabled(False)
 
     def btn_vlv2(self):
-        self.button_action(self, 2, self.vlv2_button, self.vlv2_verticalSlider)
+        self.button_action(2, self.vlv2_button, self.vlv2_verticalSlider)
         # if self.vlv2_button.text() == "VALVE2 ON":
         #     # self.sw_start(2), self.vlv2_verticalSlider.value()
         #     self.sw_init(2)
@@ -100,7 +96,7 @@ class WindowsForms(QtWidgets.QMainWindow, vd_design.Ui_MainWindow, ValveGPIO):
         #     self.vlv2_verticalSlider.setEnabled(False)
 
     def btn_vlv3(self):
-        self.button_action(self, 3, self.vlv3_button, self.vlv3_verticalSlider)
+        self.button_action(3, self.vlv3_button, self.vlv3_verticalSlider)
         # if self.vlv3_button.text() == "VALVE3 ON":
         #     # self.sw_start(3, self.vlv3_verticalSlider.value())
         #     self.sw_init(3)
@@ -112,7 +108,7 @@ class WindowsForms(QtWidgets.QMainWindow, vd_design.Ui_MainWindow, ValveGPIO):
         #     self.vlv3_verticalSlider.setEnabled(False)
 
     def btn_vlv4(self):
-        self.button_action(self, 4, self.vlv4_button, self.vlv4_verticalSlider)
+        self.button_action(4, self.vlv4_button, self.vlv4_verticalSlider)
         # if self.vlv4_button.text() == "VALVE4 ON":
         #     # self.sw_start(4, self.vlv4_verticalSlider.value())
         #     self.sw_init(4)
