e7b3ce6107f5806a4d440cc0e92e0bd8ee1c1b21
ortariot <ortariot@gmail.com>
2021-06-10 18:32:27 +0700

    change control logic

--- main.py
+++ main.py
@@ -11,87 +11,117 @@ class WindowsForms(QtWidgets.QMainWindow, vd_design.Ui_MainWindow, ValveGPIO):
         self.setupUi(self)
         ValveGPIO.__init__(self)
         self.vlv1_verticalSlider.setRange(1, 100)
-        self.vlv1_verticalSlider.setValue(50)
-        self.vlv1_lcdNumber.display(50)
+        self.vlv1_verticalSlider.setValue(1)
+        self.vlv1_lcdNumber.display(1)
         self.vlv1_verticalSlider.valueChanged.connect(self.slider_vlv1)
         self.vlv2_verticalSlider.setRange(1, 100)
-        self.vlv2_verticalSlider.setValue(50)
-        self.vlv2_lcdNumber.display(50)
+        self.vlv2_verticalSlider.setValue(1)
+        self.vlv2_lcdNumber.display(1)
         self.vlv2_verticalSlider.valueChanged.connect(self.slider_vlv2)
         self.vlv3_verticalSlider.setRange(1, 100)
         self.vlv3_verticalSlider.valueChanged.connect(self.slider_vlv3)
-        self.vlv3_verticalSlider.setValue(50)
-        self.vlv3_lcdNumber.display(50)
+        self.vlv3_verticalSlider.setValue(1)
+        self.vlv3_lcdNumber.display(1)
         self.vlv4_verticalSlider.setRange(1, 100)
-        self.vlv4_verticalSlider.setValue(50)
+        self.vlv4_verticalSlider.setValue(1)
         self.vlv4_verticalSlider.valueChanged.connect(self.slider_vlv4)
-        self.vlv4_lcdNumber.display(50)
+        self.vlv4_lcdNumber.display(1)
         self.vlv1_button.clicked.connect(self.btn_vlv1)
         self.vlv2_button.clicked.connect(self.btn_vlv2)
         self.vlv3_button.clicked.connect(self.btn_vlv3)
         self.vlv4_button.clicked.connect(self.btn_vlv4)
 
+    def button_action(self, valve_num, button_obj, slider_obj):
+        if self.button_obj.text() == f"VALVE{valve_num} ON":
+            self.sw_init(valve_num)
+            self.slider_obj.setEnabled(True)
+            self.sw_start(valve_num, self.vlv1_verticalSlider.value())
+            self.button_obj.setText(f"VALVE{valve_num} OFF")
+        elif self.button_obj.text() == f"VALVE{valve_num} OFF":
+            self.sw_stop(valve_num)
+            self.button_obj.setText(f"VALVE{valve_num} ON")
+            self.slider_obj.setEnabled(False)
+
+    def slaide_action(self, valve_num, slider_obj, dysplay_obj):
+        value_vlv1 = slider_obj.value()
+        self.dysplay_obj.display(value_vlv1)
+        self.set_duty(valve_num, value_vlv1)
+
     def slider_vlv1(self):
-        value_vlv1 = self.vlv1_verticalSlider.value()
-        self.vlv1_lcdNumber.display(value_vlv1)
-        self.set_duty(1, value_vlv1)
-   
+        self.slaide_action(self, 1, self.vlv1_verticalSlider,
+                           self.vlv1_lcdNumber)
+        # value_vlv1 = self.vlv1_verticalSlider.value()
+        # self.vlv1_lcdNumber.display(value_vlv1)
+        # self.set_duty(1, value_vlv1)
 
     def slider_vlv2(self):
-        value_vlv2 = self.vlv2_verticalSlider.value()
-        self.vlv2_lcdNumber.display(value_vlv2)
-        self.set_duty(2, value_vlv2)
-
+        self.slaide_action(self, 2, self.vlv1_verticalSlider,
+                           self.vlv1_lcdNumber)
+        # value_vlv2 = self.vlv2_verticalSlider.value()
+        # self.vlv2_lcdNumber.display(value_vlv2)
+        # self.set_duty(2, value_vlv2)
 
     def slider_vlv3(self):
-        value_vlv3 = self.vlv3_verticalSlider.value()
-        self.vlv3_lcdNumber.display(value_vlv3)
-        self.set_duty(3, value_vlv3)
+        self.slaide_action(self, 3, self.vlv1_verticalSlider,
+                           self.vlv1_lcdNumber)
+        # value_vlv3 = self.vlv3_verticalSlider.value()
+        # self.vlv3_lcdNumber.display(value_vlv3)
+        # self.set_duty(3, value_vlv3)
 
     def slider_vlv4(self):
-        value_vlv4 = self.vlv4_verticalSlider.value()
-        self.vlv4_lcdNumber.display(value_vlv4)
-        self.set_duty(4, value_vlv4)
+        self.slaide_action(self, 4, self.vlv1_verticalSlider,
+                           self.vlv1_lcdNumber)
+        # value_vlv4 = self.vlv4_verticalSlider.value()
+        # self.vlv4_lcdNumber.display(value_vlv4)
+        # self.set_duty(4, value_vlv4)
 
     def btn_vlv1(self):
-        if self.vlv1_button.text() == "VALVE1 ON":
-            self.sw_start(1, self.vlv1_verticalSlider.value())
-            self.vlv1_button.setText("VALVE1 OFF")
-            self.vlv1_verticalSlider.setEnabled(True)
-        elif self.vlv1_button.text() == "VALVE1 OFF":
-            self.sw_stop(1)
-            self.vlv1_button.setText("VALVE1 ON")
-            self.vlv1_verticalSlider.setEnabled(False)
+        self.button_action(self, 1, self.vlv1_button, self.vlv1_verticalSlider)
+        # if self.vlv1_button.text() == "VALVE1 ON":
+        #     # self.sw_start(1, self.vlv1_verticalSlider.value())
+        #     self.sw_init(1)
+        #     self.vlv1_button.setText("VALVE1 OFF")
+        #     self.vlv1_verticalSlider.setEnabled(True)
+        # elif self.vlv1_button.text() == "VALVE1 OFF":
+        #     self.sw_stop(1)
+        #     self.vlv1_button.setText("VALVE1 ON")
+        #     self.vlv1_verticalSlider.setEnabled(False)
 
     def btn_vlv2(self):
-        if self.vlv2_button.text() == "VALVE2 ON":
-            self.sw_start(2), self.vlv2_verticalSlider.value()
-            self.vlv2_button.setText("VALVE2 OFF")
-            self.vlv2_verticalSlider.setEnabled(True)
-        elif self.vlv2_button.text() == "VALVE2 OFF":
-            self.sw_stop(2)
-            self.vlv2_button.setText("VALVE2 ON")
-            self.vlv2_verticalSlider.setEnabled(False)
+        self.button_action(self, 2, self.vlv2_button, self.vlv2_verticalSlider)
+        # if self.vlv2_button.text() == "VALVE2 ON":
+        #     # self.sw_start(2), self.vlv2_verticalSlider.value()
+        #     self.sw_init(2)
+        #     self.vlv2_button.setText("VALVE2 OFF")
+        #     self.vlv2_verticalSlider.setEnabled(True)
+        # elif self.vlv2_button.text() == "VALVE2 OFF":
+        #     self.sw_stop(2)
+        #     self.vlv2_button.setText("VALVE2 ON")
+        #     self.vlv2_verticalSlider.setEnabled(False)
 
     def btn_vlv3(self):
-        if self.vlv3_button.text() == "VALVE3 ON":
-            self.sw_start(3, self.vlv3_verticalSlider.value())
-            self.vlv3_button.setText("VALVE3 OFF")
-            self.vlv3_verticalSlider.setEnabled(True)
-        elif self.vlv3_button.text() == "VALVE3 OFF":
-            self.sw_stop(3)
-            self.vlv3_button.setText("VALVE3 ON")
-            self.vlv3_verticalSlider.setEnabled(False)
+        self.button_action(self, 3, self.vlv3_button, self.vlv3_verticalSlider)
+        # if self.vlv3_button.text() == "VALVE3 ON":
+        #     # self.sw_start(3, self.vlv3_verticalSlider.value())
+        #     self.sw_init(3)
+        #     self.vlv3_button.setText("VALVE3 OFF")
+        #     self.vlv3_verticalSlider.setEnabled(True)
+        # elif self.vlv3_button.text() == "VALVE3 OFF":
+        #     self.sw_stop(3)
+        #     self.vlv3_button.setText("VALVE3 ON")
+        #     self.vlv3_verticalSlider.setEnabled(False)
 
     def btn_vlv4(self):
-        if self.vlv4_button.text() == "VALVE4 ON":
-            self.sw_start(4, self.vlv4_verticalSlider.value())
-            self.vlv4_button.setText("VALVE4 OFF")
-            self.vlv4_verticalSlider.setEnabled(True)
-        elif self.vlv4_button.text() == "VALVE4 OFF":
-            self.sw_stop(4)
-            self.vlv4_button.setText("VALVE4 ON")
-            self.vlv4_verticalSlider.setEnabled(False)
+        self.button_action(self, 4, self.vlv4_button, self.vlv4_verticalSlider)
+        # if self.vlv4_button.text() == "VALVE4 ON":
+        #     # self.sw_start(4, self.vlv4_verticalSlider.value())
+        #     self.sw_init(4)
+        #     self.vlv4_button.setText("VALVE4 OFF")
+        #     self.vlv4_verticalSlider.setEnabled(True)
+        # elif self.vlv4_button.text() == "VALVE4 OFF":
+        #     self.sw_stop(4)
+        #     self.vlv4_button.setText("VALVE4 ON")
+        #     self.vlv4_verticalSlider.setEnabled(False)
 
 
 def main():
--- valve_gpio.py
+++ valve_gpio.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+# as varuant https://github.com/metachris/RPIO 
+# https://pypi.org/project/RPIO/
 import sys
 
 
@@ -13,24 +15,34 @@ class ValveGPIO():
         pin_valve_three = 27
         pin_valve_for = 5
         # GPIO pin inicialization as OUT
-        GPIO.setup(pin_valve_one, GPIO.OUT)
-        GPIO.setup(pin_valve_two, GPIO.OUT)
-        GPIO.setup(pin_valve_three, GPIO.OUT)
-        GPIO.setup(pin_valve_for, GPIO.OUT)
+        # GPIO.setup(pin_valve_one, GPIO.OUT)
+        # GPIO.setup(pin_valve_two, GPIO.OUT)
+        # GPIO.setup(pin_valve_three, GPIO.OUT)
+        # GPIO.setup(pin_valve_for, GPIO.OUT)
         # OUT inicialization as PWM object
-        self.valve_one = GPIO.PWM(pin_valve_one, 1)
-        self.valve_two = GPIO.PWM(pin_valve_two, 1)
-        self.valve_three = GPIO.PWM(pin_valve_three, 1)
-        self.valve_for = GPIO.PWM(pin_valve_for, 1)
+        # self.valve_one = GPIO.PWM(pin_valve_one, 1)
+        # self.valve_two = GPIO.PWM(pin_valve_two, 1)
+        # self.valve_three = GPIO.PWM(pin_valve_three, 1)
+        # self.valve_for = GPIO.PWM(pin_valve_for, 1)
         # dict for valve independent control
-        self.valve_store = {1: self.valve_one,
-                            2: self.valve_two,
-                            3: self.valve_three,
-                            4: self.valve_for
+        self.pin_store = {1: pin_valve_one,
+                          2: pin_valve_two,
+                          3: pin_valve_three,
+                          4: pin_valve_for
+                          }
+        # self.valve_store = {1: self.valve_one,
+        #                     2: self.valve_two,
+        #                     3: self.valve_three,
+        #                     4: self.valve_for
+        #                     }
+        self.valve_store = {1: None,
+                            2: None,
+                            3: None,
+                            4: None
                             }
-        for valve in self.valve_store.values():
-            valve.stop()
-            valve.ChangeFrequency(0.5)
+        # for valve in self.valve_store.values():
+        #     valve.stop()
+        #     valve.ChangeFrequency(0.5)
 
     def set_duty(self, valve_num, duty):
         self.valve_store[valve_num].ChangeDutyCycle(duty)
@@ -45,8 +57,15 @@ class ValveGPIO():
     def sw_start(self, valve_num, duty=50):
         self.valve_store[valve_num].start(duty)
 
+    def sw_init(self, valve_num):
+        GPIO.setup(self.pin_store[valve_num], GPIO.OUT)
+        GPIO.output(self.pin_store[valve_num], False)
+        self.valve_store[valve_num] = GPIO.PWM(self.pin_store[valve_num], 1)
+        self.valve_store[valve_num].ChangeFrequency(0.5)
+
     def sw_stop(self, valve_num):
         self.valve_store[valve_num].stop()
+        GPIO.cleanup(self.pin_store[valve_num])
 
 
 def main():
