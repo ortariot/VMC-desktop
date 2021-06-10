a17b4e327da19de82ef6d66203e8f37202298c11
ortariot <ortariot@gmail.com>
2021-06-10 19:06:28 +0700

    error edit

--- valve_gpio.py
+++ valve_gpio.py
@@ -14,35 +14,16 @@ class ValveGPIO():
         pin_valve_two = 3
         pin_valve_three = 27
         pin_valve_for = 5
-        # GPIO pin inicialization as OUT
-        # GPIO.setup(pin_valve_one, GPIO.OUT)
-        # GPIO.setup(pin_valve_two, GPIO.OUT)
-        # GPIO.setup(pin_valve_three, GPIO.OUT)
-        # GPIO.setup(pin_valve_for, GPIO.OUT)
-        # OUT inicialization as PWM object
-        # self.valve_one = GPIO.PWM(pin_valve_one, 1)
-        # self.valve_two = GPIO.PWM(pin_valve_two, 1)
-        # self.valve_three = GPIO.PWM(pin_valve_three, 1)
-        # self.valve_for = GPIO.PWM(pin_valve_for, 1)
-        # dict for valve independent control
         self.pin_store = {1: pin_valve_one,
                           2: pin_valve_two,
                           3: pin_valve_three,
                           4: pin_valve_for
                           }
-        # self.valve_store = {1: self.valve_one,
-        #                     2: self.valve_two,
-        #                     3: self.valve_three,
-        #                     4: self.valve_for
-        #                     }
         self.valve_store = {1: None,
                             2: None,
                             3: None,
                             4: None
                             }
-        # for valve in self.valve_store.values():
-        #     valve.stop()
-        #     valve.ChangeFrequency(0.5)
 
     def set_duty(self, valve_num, duty):
         self.valve_store[valve_num].ChangeDutyCycle(duty)
